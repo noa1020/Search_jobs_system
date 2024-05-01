f1721e87db51dff9e4e6d205f219cdcf4bae3ec8
noa1020 <noa0533181648@gmail.com>
2024-05-01 18:33:02 +0300

    add services & models

---
+++ client side/SearchJobSystem/src/app/Models/FieldJob.ts
@@ -0,0 +1,4 @@
+export interface Job{
+    jobFieldId:number
+    jobFieldName:string
+}
---
+++ client side/SearchJobSystem/src/app/Services/User.Service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Observable, Observer } from 'rxjs';
+import { User } from '../Models/User';
+
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class UserService {
+    constructor(private http: HttpClient) {
+        this.GetUsers();
+    }
+
+    UserList: User[] = []
+
+    public GetUserList() {
+        return this.UserList
+    }
+
+    GetUsers() {
+        this.http.get('https://localhost:7231/User').subscribe((res: any) => this.UserList = res)
+    }
+
+    addUser(user: User) {
+        this.UserList.push(user)
+        this.http.post('https://localhost:7231/User', { body: user }).subscribe(res => { })
+    }
+
+
+}
