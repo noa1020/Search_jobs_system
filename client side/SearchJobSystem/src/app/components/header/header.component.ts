7a081041f4b3dacd28503c73124ff460fcfed7fd
noa1020 <noa0533181648@gmail.com>
2024-05-03 04:59:19 +0300

    add field job to the header

--- client side/SearchJobSystem/src/app/components/header/header.component.ts
+++ client side/SearchJobSystem/src/app/components/header/header.component.ts
@@ -2,40 +2,23 @@ import { User } from '../../Models/User';
 import { FieldJob } from '../../Models/FieldJob';
 import { FieldJobService } from '../../Services/FieldJob.service';
 import { Router } from '@angular/router';
-//
-//@Component({
-//  selector: 'app-header',
-//  templateUrl: './header.component.html',
-//  styleUrl: './header.component.scss'
-//})
-//export class HeaderComponent {
-//  constructor(private fieldJobService: FieldJobService, private router: Router) { }
-//  user: User = JSON.parse(localStorage.getItem("user") || '{}');
-//  CVsNumber: number = this.user.cVsSentCount;
-//  fieldJobName: string
-//  this.fieldJobService.GetFieldJobById(this.user.fieldJobId).subscribe((fieldJob: FieldJob | null) => {
-//    if (fieldJob !== null) {
-//      this.fieldJobName = fieldJob.jobFieldName;
-//    });
-//
-//}
-//
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { catchError } from 'rxjs/operators';
-import { of } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrl: './header.component.scss'
 })
+
 export class HeaderComponent implements OnInit {
-  user: User = JSON.parse(localStorage.getItem("user") || '{}');
-  CVsNumber: number | undefined = this.user.cVsSentCount;
+  user!: User;
+  CVsNumber: number | undefined ;
   fieldJobName: string | undefined;
   constructor(private fieldJobService: FieldJobService, private router: Router) { }
   ngOnInit(): void {
+    this.user = JSON.parse(localStorage.getItem("user") || '{}');
+    this.CVsNumber = this.user.cVsSentCount;
+
     this.getFieldJobDetails();
   }
   getFieldJobDetails() {
