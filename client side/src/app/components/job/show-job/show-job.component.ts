0adcdd0a548438e156d1fc84414334a5ae33181f
noa1020 <noa0533181648@gmail.com>
2024-05-05 08:26:02 +0300

    add jobFieldName to job

--- client side/src/app/components/job/show-job/show-job.component.ts
+++ client side/src/app/components/job/show-job/show-job.component.ts
@@ -1,14 +1,24 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Job } from '../../../models/job.model';
+import { JobFieldService } from '../../../services/jobField.service';
+import { JobField } from '../../../models/jobField.models';
 
 @Component({
   selector: 'app-show-job',
   templateUrl: './show-job.component.html',
   styleUrl: './show-job.component.scss'
 })
-export class ShowJobComponent {
+export class ShowJobComponent implements OnInit {
   @Input() job: Job | undefined;
+  jobFieldName?:string;
 
-  constructor() { }
+  constructor(private jobFieldService: JobFieldService) { }
 
+  ngOnInit() {
+    if (this.job?.jobFieldId) {
+      this.jobFieldService.getJobFieldById(this.job.jobFieldId).subscribe((jobField: JobField | undefined) => {
+        this.jobFieldName = jobField?.jobFieldName;
+      });
+    }
+  }
 }
